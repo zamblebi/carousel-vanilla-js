3a6c486c56a0b2a05c018bfd817895837d6fc09d
Samuel Bouye <bouyesamuel@gmail.com>
2020-07-04 14:18:45 +0000

    slider first step

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+let element = document.querySelector('.main')
+let allElement = document.querySelectorAll('.carousel-item')
+
+var i;
+for(i = 0; i < allElement.length; i++){
+    allElement[i].style.position = "absolute"
+}
+
+var per = 0
+
+
+
+console.log(element.style)
+element.addEventListener("click", (e)=> {
+    // element.style.transform = `translate3d(-${ per === 300 ? null : per += 100}%)`
+    element.style.transform = `translateX(-${ per === 300 ? null : per += 100}%)`
+    console.log('click')
+})
